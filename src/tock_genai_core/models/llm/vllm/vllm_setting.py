06d37a65a9548258b5a1d0b807086163537f9909
f5235 <baptiste.le-goff@arkea.com>
2025-04-15 16:21:48 +0200

    black

--- src/tock_genai_core/models/llm/vllm/vllm_setting.py
+++ src/tock_genai_core/models/llm/vllm/vllm_setting.py
@@ -21,7 +21,8 @@ from tock_genai_core.models.llm.setting import BaseLLMSetting
 
 class VllmSetting(BaseLLMSetting):
     """
-    Configuration settings for the VLLM (Qwen) LLM integration.  
+    Configuration settings for the VLLM (Qwen) LLM integration.
+
     This class defines the configuration required to connect to the VLLM API.
 
     Attributes
