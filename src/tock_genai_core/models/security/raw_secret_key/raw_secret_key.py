6bdd99b8e4d338e8e40ba9e3ce88f99037789039
Louis <louis.toudoire@gmail.com>
2025-08-21 11:48:26 +0200

    Black

--- src/tock_genai_core/models/security/raw_secret_key/raw_secret_key.py
+++ src/tock_genai_core/models/security/raw_secret_key/raw_secret_key.py
@@ -38,9 +38,4 @@ class RawSecretKey(BaseSecretKey):
         examples=[SecretKeyType.RAW],
         default=SecretKeyType.RAW,
     )
-    secret: str = Field(
-        description="The secret value.",
-        examples=["145d-ff455g-e4r5gf"],
-        min_length=1,
-        alias="value"
-    )
+    secret: str = Field(description="The secret value.", examples=["145d-ff455g-e4r5gf"], min_length=1, alias="value")
